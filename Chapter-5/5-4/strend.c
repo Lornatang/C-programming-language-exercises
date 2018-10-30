c057e5a31cb0f5264ca18d6784ed092130faa1fb
Lornatang <shiyipaisizuo@gmail.com>
2018-10-30 12:26:27 +0800

    update

--- Chapter-5/5-4/strend.c
+++ Chapter-5/5-4/strend.c
@@ -15,27 +15,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* strend:  returns 1 if the string t occurs at the end of the string s,
-   and zero otherwise */
-int strend(const char *s, const char *t) {
-  int ls, lt;
+/* strend:  returns 1 if the string t
+ *  occurs at the end of the string s,and zero otherwise
+ */
 
-  for (ls = 0; *(s + ls); ++ls)
-    ; /* find length of s */
-  for (lt = 0; *(t + lt); ++lt)
-    ;             /* find length of t */
-  if (ls >= lt) { /* check if t can fit in s */
-    s += ls - lt; /* point s to where t should start */
+int strEnd(const char *s, const char *t) {
+  int ls, lt;
+  for (ls = 0; *(s + ls); ls += 1)
+    ;
+  for (lt = 0; *(t + lt); lt += 1)
+    ;
+  if (ls >= lt) {
+    s += ls - lt;
     while (*s++ == *t++)
-      if (!*s)    /* we found end of s and therefore t */
-        return 1; /* so return 1 */
+      if (!*s) return 1;
   }
   return 0;
 }
+//   if (ls >= lt) { /* check if t can fit in s */
+//     s += ls - lt; /* point s to where t should start */
+//     while (*s++ == *t++)
+//       if (!*s)    /* we found end of s and therefore t */
+//         return 1; /* so return 1 */
+//   }
+//   return 0;
+// }
 
-int main(void) {
+int main() {
   char s[100] = "abcdef";
-  char t[100] = "def";
-  printf("%d\n", strend(s, t));
+  char t[100] = "bc";
+  printf("%d\n", strEnd(s, t));
   return 0;
 }
